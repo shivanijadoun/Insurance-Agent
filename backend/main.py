d91e09035ae680fe300df9b4fde3f5415436321d
Shivani <shivanirana0080@gmail.com>
2026-06-29 00:49:01 +0530

    Update frontend to use deployed backend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://insurance-agent-humq.vercel.app/",
+    "https://insurance-agent-humq.vercel.app",
 ],
     allow_credentials=True,
     allow_methods=["*"],
